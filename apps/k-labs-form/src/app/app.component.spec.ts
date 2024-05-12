21f2c07c68f042bf6f6531a84343654fbb7e701f
Wataru.Kasahara <kasahara1019@gmail.com>
2024-05-12 13:33:08 +0900

    refactor: Delete default template

--- apps/k-labs-form/src/app/app.component.spec.ts
+++ apps/k-labs-form/src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -14,14 +12,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome k-labs-form'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Forms');
   });
 
-  it(`should have as title 'k-labs-form'`, () => {
+  it(`should have as title 'Forms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('k-labs-form');
+    expect(app.title).toEqual('Forms');
   });
 });
